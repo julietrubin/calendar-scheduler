dd542118718f9f516d4beb3ccffab60e546e5455
julietrubin <rubin.juliet@gmail.com>
2025-03-22 11:46:24 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { buttonVariants } from "@/components/ui/button";
 
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen space-y-20">
+    <main className="flex text-red-50 flex-col items-center justify-center min-h-screen space-y-20">
       <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
         Vite, React, Shadcn-ui minimal starter
       </h1>
